abda1ea71de58e0d22a75779c92fd0ad654ef709
SlavkoV <slavko.vuletic92@gmail.com>
2020-12-23 15:24:03 -0800

    cache removed

--- testScripts/bat/daily_reboot_at12pm.bat
+++
@@ -1,3 +0,0 @@
-schtasks /create /tn "Weekly Reboot" /tr "shutdown.exe -r -t 300 -c 'Initiating scheduled reboot for system maintenance. You have 5 minutes to save your work before mandatory forced reboot.'" /sc daily /st 12:00
-
-pause
